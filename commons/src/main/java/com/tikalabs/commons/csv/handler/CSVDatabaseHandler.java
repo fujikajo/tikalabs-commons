074ff1e225005e577a4be39f7e25361a0a2a68da
fujikajo <160280210+fujikajo@users.noreply.github.com>
2024-03-25 18:36:06 +0100

    Update CSVDatabaseHandler.java

--- commons/src/main/java/com/tikalabs/commons/csv/handler/CSVDatabaseHandler.java
+++ commons/src/main/java/com/tikalabs/commons/csv/handler/CSVDatabaseHandler.java
@@ -7,57 +7,66 @@ import com.tikalabs.commons.database.servicelayer.EmbeddedDBService;
 
 public class CSVDatabaseHandler {
 
-    private final EmbeddedDBService service;
-    private CSVDatabaseRecordProcessor recordProcessor;
-    private CustomCSVParser csvParser;
-    private String tableName;
+	private final EmbeddedDBService service;
+	private CSVDatabaseRecordProcessor recordProcessor;
+	private CustomCSVParser csvParser;
+	private String tableName;
+	// Eine Map, um die Reihenfolge der Header basierend auf dem Tabellennamen zu
+	// speichern
+	private Map<String, List<String>> headerOrderMap = new HashMap<>();
 
-    public CSVDatabaseHandler(EmbeddedDBService service) {
-        this.service = service;
-    }
+	public CSVDatabaseHandler(EmbeddedDBService service) {
+		this.service = service;
+	}
 
-    public String getTableName() {
-        return tableName;
-    }
+	public String getTableName() {
+		return tableName;
+	}
 
-    public void setTableName(String tableName) {
-        this.tableName = tableName;
-    }
+	public void setTableName(String tableName) {
+		this.tableName = tableName;
+	}
 
-    public void execute() {
-        this.service.createTable(getTableName(), getCsvParser().getHeaderNames());
-        this.getCsvParser().parseCSV();
+	public void execute() {
+		this.service.createTable(getTableName(), getCsvParser().getHeaderNames());
+		this.getCsvParser().parseCSV();
 
-    }
+	}
 
-    public void load(String tableName, String csvFilePath, int headerIndex) {
+	public void load(String tableName, String csvFilePath, int headerIndex) {
 
+		// Setze den Tabellennamen und den Dateipfad für den aktuellen Ladevorgang
+		this.setTableName(tableName);
+		this.setRecordProcessor(new CSVDatabaseRecordProcessor(tableName, this.service));
+		this.setCsvParser(new CustomCSVParser(csvFilePath, this.recordProcessor, headerIndex));
 
-        // Setze den Tabellennamen und den Dateipfad für den aktuellen Ladevorgang
-        this.setTableName(tableName);
-        this.setRecordProcessor(new CSVDatabaseRecordProcessor(tableName, this.service));
-        this.setCsvParser(new CustomCSVParser(csvFilePath, this.recordProcessor, headerIndex)); // Angenommen, der headerIndex ist immer 8, anpassen falls nötig
+		// Speichere die Reihenfolge der Header für die aktuelle Tabelle
+		this.headerOrderMap.put(tableName, this.csvParser.getHeaderNames());
 
-        // Erstelle die Tabelle in der Datenbank und lade die CSV-Daten
-        this.service.createTable(getTableName(), getCsvParser().getHeaderNames());
-        this.getCsvParser().parseCSV();
-    }
+		// Erstelle die Tabelle in der Datenbank und lade die CSV-Daten
+		this.service.createTable(getTableName(), getCsvParser().getHeaderNames());
+		this.service.createIndex(getTableName(), "idx_vers_nr_" + tableName);
+		this.getCsvParser().parseCSV();
+	}
 
+	public List<String> getHeaderOrder(String tableName) {
+		return headerOrderMap.get(tableName);
+	}
 
-    public CSVDatabaseRecordProcessor getRecordProcessor() {
-        return recordProcessor;
-    }
+	public CSVDatabaseRecordProcessor getRecordProcessor() {
+		return recordProcessor;
+	}
 
-    public void setRecordProcessor(CSVDatabaseRecordProcessor recordProcessor) {
-        this.recordProcessor = recordProcessor;
-    }
+	public void setRecordProcessor(CSVDatabaseRecordProcessor recordProcessor) {
+		this.recordProcessor = recordProcessor;
+	}
 
-    public CustomCSVParser getCsvParser() {
-        return csvParser;
-    }
+	public CustomCSVParser getCsvParser() {
+		return csvParser;
+	}
 
-    public void setCsvParser(CustomCSVParser csvParser) {
-        this.csvParser = csvParser;
-    }
+	public void setCsvParser(CustomCSVParser csvParser) {
+		this.csvParser = csvParser;
+	}
 
 }
